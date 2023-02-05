5a0d3d80373ed069c42a326809a414d69a4782b1
missekong <missekong@gmail.com>
2023-02-05 00:28:50 -0600

    commit

--- src/EmailApp/emailApp.java
+++ src/EmailApp/emailApp.java
@@ -4,6 +4,7 @@ public class emailApp {
     public static void main(String[] args) {
         //create an object called em1// use println on email class to test
         email em1 = new email("John", "Smith");
+        System.out.println( em1.showInfo());
 
     }
 }
